0f89b690ed558e46abdda3ab229387c0c9094ae4
Justin Zou <justinzo@andrew.cmu.edu>
2025-01-23 14:08:29 -0500

    new helper function check res reducing by 4 cognitive complexity again

--- src/controllers/category.js
+++ src/controllers/category.js
@@ -29,6 +29,9 @@ async function checkCategories(categoryFields,currentPage) {
 	return (!categoryFields.slug || (categoryFields && categoryFields.disabled) || (userSettings.usePagination && currentPage < 1))
 }
 
+async function checkRes(res,req,categoryFields,cid) {
+	return !res.locals.isAPI && !req.params.slug && (categoryFields.slug && categoryFields.slug !== `${cid}/`)	
+}
 
 categoryController.get = async function (req, res, next) {
 	const cid = req.params.category_id;
@@ -58,7 +61,7 @@ categoryController.get = async function (req, res, next) {
 		return helpers.notAllowed(req, res);
 	}
 
-	if (!res.locals.isAPI && !req.params.slug && (categoryFields.slug && categoryFields.slug !== `${cid}/`)) {
+	if (checkRes(res,req,categoryFields,cid)) {
 		return helpers.redirect(res, `/category/${categoryFields.slug}?${qs.stringify(req.query)}`, true);
 	}
 
